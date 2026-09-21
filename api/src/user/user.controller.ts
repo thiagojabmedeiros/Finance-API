c3841f25989d621095cfc8c1108f1917fd0a94c6
Thiago Medeiros <thiagojabm@gmail.com>
2026-09-21 01:22:53 -0300

    fix: use validationPipe in main

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDto } from './dto';
 
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
-
+    @Post()
+    create(@Body() userDto: UserDto) {
+        return this.userService.createUser(userDto.username, userDto.email, userDto.password)
+    }
 }
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -4,5 +4,20 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class UserService {
     constructor(private readonly prisma: PrismaService) {}
-    
+    async createUser(username: string, email: string, password: string) {
+        const user = await this.prisma.user.create({
+            data: {
+                username: username,
+                email: email,
+                password: password
+            },
+            select: {
+                id: true,
+                username: true,
+                createdAt: true
+            }
+        })
+
+        return user
+    }
 }
