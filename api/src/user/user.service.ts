d98d4013aeffe436ecbf324d954d56be95790e83
Thiago Medeiros <thiagojabm@gmail.com>
2026-09-22 16:32:37 -0300

    refactor: create user method

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,24 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dtos';
+import { Prisma } from '../generated/prisma/client';
 
 @Injectable()
 export class UserService {
     constructor(private readonly prisma: PrismaService) {}
     async createUser(dto: CreateUserDto) {
-        const user = await this.prisma.user.create({
-            data: {
-                username: dto.username,
-                email: dto.email,
-                password: dto.password
-            },
-            select: {
-                id: true,
-                username: true,
-                createdAt: true
+        try {
+            return await this.prisma.user.create({ 
+                data: {
+                    email: dto.email,
+                    username: dto.username,
+                    password: dto.password
+                },
+                select: {
+                    id: true,
+                    role: true,
+                    username: true,
+                    createdAt: true
+                }
+            })
+        } catch(error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                if (error.code === "P2002") {
+                    throw new ConflictException("This e-mail has been already used.")
+                }
             }
-        })
-
-        return user
+            throw error
+        }
     }
 }
